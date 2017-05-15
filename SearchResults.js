f53a5d0a2e0a9f1a4fe7c92190d5c51b6481795b
John Slota <john.slota@gmail.com>
2017-05-14 18:12:31 -0700

    sweet styling added

--- SearchResults.js
+++ SearchResults.js
@@ -10,6 +10,33 @@ import {
   Text
 } from 'react-native';
 
+var styles = StyleSheet.create({
+  thumb: {
+    width: 80,
+    height: 80,
+    marginRight: 10
+  },
+  textContainer: {
+    flex: 1
+  },
+  separator: {
+    height: 1,
+    backgroundColor: '#dddddd'
+  },
+  price: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: '#48BBEC'
+  },
+  title: {
+    fontSize: 20,
+    color: '#656565'
+  },
+  rowContainer: {
+    flexDirection: 'row',
+    padding: 10
+  }
+});
 
 class SearchResults extends Component {
 
@@ -23,14 +50,24 @@ class SearchResults extends Component {
   }
 
   renderRow(rowData, sectionID, rowID) {
+    var price = rowData.price_formatted.split(' ')[0];
+
     return (
-      <TouchableHighlight
+      <TouchableHighlight onPress={() => this.rowPressed(rowData.lister_url)}
           underlayColor='#dddddd'>
         <View>
-          <Text>{rowData.title}</Text>
+          <View style={styles.rowContainer}>
+            <Image style={styles.thumb} source={{ uri: rowData.img_url }} />
+            <View  style={styles.textContainer}>
+              <Text style={styles.price}>{price}</Text>
+              <Text style={styles.title}
+                    numberOfLines={1}>{rowData.title}</Text>
+            </View>
+          </View>
+          <View style={styles.separator}/>
         </View>
       </TouchableHighlight>
-    );
+    )
   }
 
   render() {
@@ -40,6 +77,10 @@ class SearchResults extends Component {
         renderRow={this.renderRow.bind(this)}/>
     );
   }
+
+  rowPressed(listerURL) {
+  var property = this.props.listings.filter(prop => prop.lister_url === listerURL)[0];
+}
 
 }
 
